889f1986ef14b04c25a9814e721efe5b8f736df8
keithrowles <75356854+keithrowles@users.noreply.github.com>
2023-01-01 05:53:10 +1000

    Created Original Application

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,33 @@
-console.log('Server...');
+const express = require('express')
+const colors = require('colors')
+const dotenv = require('dotenv').config()
+const {errorHandler} = require('./middleware/errorMiddleware')
+const connectDB = require('./config/db')
+const PORT = process.env.PORT || 8080
+
+
+// Connect to database
+connectDB()
+
+
+const app = express()
+
+
+// Middleware
+app.use(express.json())
+app.use(express.urlencoded({extended: false}))
+
+
+// Routes
+app.get('/', (req, res) => {
+    res.status(200).json({ message: 'Welcome to the Support Desk API' })
+})
+
+app.use('/api/users', require('./routes/userRoutes'))
+app.use('/api/tickets', require('./routes/ticketRoutes'))
+
+app.use(errorHandler)
+
+
+// Port
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`))
